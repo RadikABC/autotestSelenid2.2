c08a1c297d82b4092e81fb15cfd9a1d94cdbef08
Radik <radik.red.snow@yandex.ru>
2023-09-01 11:48:10 +0400

    changeTest

--- src/test/java/ru/netology/CardDeliveryOrderFormTest.java
+++ src/test/java/ru/netology/CardDeliveryOrderFormTest.java
@@ -1,5 +1,6 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Config;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
@@ -10,26 +11,40 @@ import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
 import java.awt.event.KeyEvent;
+import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class CardDeliveryOrderFormTest {
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @Test
     void shouldTest() {
+
+        String planningDate = generateDate(4);
+
+
         Configuration.holdBrowserOpen = true;
-        Configuration.timeout = 14;
         open("http://localhost:9999");
+
         $("[data-test-id=city] input").setValue("Ульяновск");
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL, Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue("10.09.2023");
+
+        $("[data-test-id=date] input").setValue(planningDate);
 
         $("[data-test-id=name] input").setValue("Баранов Василий");
         $("[data-test-id=phone] input").setValue("+79270002277");
         $("[data-test-id=agreement]").click();
         $x("//div//span[text()='Забронировать']").click();
-        $x("[data-test-id=notification]").shouldHave(exactText("Встреча успешно забронирована"));
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate));
     }
 }
 
